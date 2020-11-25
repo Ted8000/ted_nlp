f7c5a55e9096ae8c2b3dfc69d996943722db0368
lupinsu001 <lupinsu001@jx-bd-hadoop38.zeus.lianjia.com>
2020-11-25 20:49:20 +0800

    1

--- pre_trainer.py
+++ pre_trainer.py
@@ -1,16 +1,15 @@
 # 预训练
-%%time
 from transformers import *
-
+import torch
 
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-chinese')
-dataset_train = LineByLineTextDataset(tokenizer, 'vr_broker/data/raw_data/ssc910_train.txt', block_size=128)
+dataset_train = LineByLineTextDataset(tokenizer, '/home/lupinsu001/data/raw_data/vakan_1000', block_size=32)
 
 print(len(dataset_train))
 
 config = RobertaConfig(
     vocab_size=21128,
-    max_position_embeddings=512,
+    max_position_embeddings=32,
     num_attention_heads=6,
     num_hidden_layers=4,
     type_vocab_size=1,
@@ -28,7 +27,7 @@ training_args = TrainingArguments(
     output_dir="./EsperBERTo",
     overwrite_output_dir=True,
     num_train_epochs=1,
-    per_gpu_train_batch_size=64,
+    per_device_train_batch_size=128,
     save_steps=10_000,
     save_total_limit=2,
 )
@@ -44,4 +43,5 @@ trainer = Trainer(
 trainer.train()
 
 # save model
-# trainer.save_model('./data/models/pretrained_model')
+trainer.save_model('data/models/pretrained_model')
+torch.save(model, 'pre_train_model.pt')
